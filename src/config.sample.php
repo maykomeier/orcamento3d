770b5d30d355804e792e737292134fce7b8d4d0a
Mayko Nestor Meier <maykomeier@gmail.com>
2026-01-13 10:30:24 -0300

    Add files via upload

---
+++ src/config.sample.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', '10.0.0.253');
+define('DB_NAME', 'orcamentos');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_PORT', 3306);
+define('DB_CHARSET', 'utf8mb4');
